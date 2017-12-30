2e37fdd25050ab249c0fa14ce9b4d1c61ae6f2e7
WMXPY <wm@mengw.io>
2017-12-29 19:19:14 -0600

    packed

--- example/main.js
+++ example/main.js
@@ -1,10 +1,8 @@
 const {
-    Hello,
-    IsNumber,
-    IsArray
+    Hello
 } = require("../build/Release/fv.node"); // native c++
 const isPrime = require('./isPrime.js'); // js
 
-// console.log(Hello());
+console.log(Hello(3));
 // console.log(IsNumber("450"))
 // console.log(IsArray(["450"]))
--- src/main.cc
+++ src/main.cc
@@ -1,31 +1,85 @@
-#include <nan.h>
 #include <node.h>
-#include "./verifys.cc"
+#include <v8.h>
+#include <nan.h>
 
-NAN_METHOD(Hello)
+#include "verifys.cc"
+
+using namespace v8;
+
+/**
+ * Without NAN
+ */
+// Handle<Value> Hello(const Arguments &args)
+// {
+//     HandleScope scope;
+//     return scope.Close(String::New("Hello from C++!"));
+// }
+
+/**
+ * Hard packaged way with NAN
+ */
+// NAN_METHOD(Hello)
+// {
+//     auto message = Nan::New("Hello!").ToLocalChecked();
+//     info.GetReturnValue().Set(message);
+//     return;
+// }
+
+/**
+ * With Nan
+ */
+void Hello(const Nan::FunctionCallbackInfo<Value> &info)
 {
-    auto message = Nan::New("Hello from C++!").ToLocalChecked();
+    auto message = Nan::New("Hello!").ToLocalChecked();
     info.GetReturnValue().Set(message);
     return;
 }
 
-NAN_MODULE_INIT(Initialize)
+/**
+ * Without NAN
+ */
+// void init(Handle<object> target)
+// {
+//     NODE_SET_METHOD(target, "Hello", Hello);
+// }
+
+/**
+ * With NAN
+ */
+void Init(Local<Object> exports)
 {
-    NAN_EXPORT(target, Hello);
-    // From verifys.cc
-    NAN_EXPORT(target, IsPrime);
-    NAN_EXPORT(target, IsPrimeExample);
-    NAN_EXPORT(target, IsArray);
-    NAN_EXPORT(target, IsNumber);
-    NAN_EXPORT(target, IsFunction);
-    NAN_EXPORT(target, IsString);
-    NAN_EXPORT(target, IsObject);
-    NAN_EXPORT(target, IsDate);
-    NAN_EXPORT(target, IsBoolean);
-    NAN_EXPORT(target, IsNativeError);
-    NAN_EXPORT(target, IsRegExp);
-    NAN_EXPORT(target, IsNull);
-    NAN_EXPORT(target, IsUndefined);
+    exports->Set(Nan::New("Hello").ToLocalChecked(), Nan::New<FunctionTemplate>(Hello)->GetFunction());
 }
 
-NODE_MODULE(fv, Initialize);
+/**
+ * Hard packaged way with NAN
+ */
+// NAN_MODULE_INIT(Initialize)
+// {
+
+//     // NAN_EXPORT(target, Hello);
+//     // From verifys.cc
+//     NAN_EXPORT(target, IsPrime);
+//     NAN_EXPORT(target, IsPrimeExample);
+//     NAN_EXPORT(target, IsArray);
+//     NAN_EXPORT(target, IsNumber);
+//     NAN_EXPORT(target, IsFunction);
+//     NAN_EXPORT(target, IsString);
+//     NAN_EXPORT(target, IsObject);
+//     NAN_EXPORT(target, IsDate);
+//     NAN_EXPORT(target, IsBoolean);
+//     NAN_EXPORT(target, IsNativeError);
+//     NAN_EXPORT(target, IsRegExp);
+//     NAN_EXPORT(target, IsNull);
+//     NAN_EXPORT(target, IsUndefined);
+// }
+
+/**
+ * Without NAN
+ */
+// NODE_MODULE(fv, init);
+
+/**
+ * With NAN
+ */
+NODE_MODULE(fv, Init);
