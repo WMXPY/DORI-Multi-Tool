9ea2d81fbb8258c528c8adb5b4be2cc5930fc4a1
WMXPY <wm@mengw.io>
2018-07-16 19:20:37 -0500

    init similar

--- src/smart/smart.cc
+++ src/smart/smart.cc
@@ -1,11 +1,21 @@
 #include <nan.h>
 #include <node.h>
+#include <string>
 
 using namespace v8;
 
+/**
+ * Levenshtein Distance 
+ */
 void Similar(const Nan::FunctionCallbackInfo<Value> &info)
 {
-    auto message = Nan::New("Similar!").ToLocalChecked();
+
+    Isolate *isolate = info.GetIsolate();
+
+    Local<String> source = Local<String>::Cast(info[0]);
+    Local<String> target = Local<String>::Cast(info[1]);
+
+    auto message = Nan::New("Similar").ToLocalChecked();
     info.GetReturnValue().Set(message);
     return;
 }
